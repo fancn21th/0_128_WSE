67708a6af91654d9b5195a949dbbef664c110909
范轶洁 <fanyjb@digitalchina.com>
2020-03-07 17:56:37 +0800

    hbs in place of handlebars

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const app = express();
 const port = 3000;
 //Loads the handlebars module
 const handlebars = require("express-handlebars");
-//Sets our app to use the handlebars engine
-app.set("view engine", "handlebars");
-//Sets handlebars configurations (we will go through them later on)
+//instead of app.set('view engine', 'handlebars');
+app.set("view engine", "hbs");
+//instead of app.engine('handlebars', handlebars({
 app.engine(
-  "handlebars",
+  "hbs",
   handlebars({
-    layoutsDir: __dirname + "/views/layouts"
+    layoutsDir: __dirname + "/views/layouts",
+    //new configuration parameter
+    extname: "hbs"
   })
 );
+
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
