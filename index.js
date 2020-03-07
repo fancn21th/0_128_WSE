122c62cd25ba96df722fa41104886825cb1b68bb
范轶洁 <fanyjb@digitalchina.com>
2020-03-07 21:02:34 +0800

    comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-//Loads the handlebars module
+// 模版引擎
 const handlebars = require("express-handlebars");
 app.set("view engine", "hbs");
 app.engine(
@@ -12,13 +12,13 @@ app.engine(
     defaultLayout: "default"
   })
 );
-
+// 静态文件
 app.use(express.static("public"));
-
+// 子路由
 var homeRouter = require("./routes/home");
 app.use("/", homeRouter);
 
 var userRouter = require("./routes/users");
 app.use("/user", userRouter);
-
+// 端口监听
 app.listen(port, () => console.log(`App listening to port ${port}`));
